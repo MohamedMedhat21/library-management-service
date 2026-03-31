4f2da829393392ab1dee5e9da861a9b083d3ed18
mohamed medhat <mohamedmmedhat21@gmail.com>
2026-03-31 05:31:27 +0200

    global exception handling

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,20 @@ import {
   ValidationPipe,
   VersioningType,
 } from '@nestjs/common';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
 
   app.enableVersioning({
     type: VersioningType.URI,
